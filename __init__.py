47283e0ec4174208bdea09fa92184a9d9aac749d
Vianne Heusdens <36879731+Vianne92@users.noreply.github.com>
2018-10-18 17:02:54 +0200

    Turn eye color back to original

--- __init__.py
+++ __init__.py
@@ -1,15 +1,10 @@
 # TODO: Add an appropriate license to your skill before publishing.  See
 # the LICENSE file for more information.
 
-# Below is the list of outside modules you'll be using in your skill.
-# They might be built-in to Python, from mycroft-core or from external
-# libraries.  If you use an external library, be sure to include it
-# in the requirements.txt file so the library is installed properly
-# when the skill gets installed later by a user.
-
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import getLogger
+from time import sleep
 
 __author__ = 'Vianne'
 LOGGER = getLogger(__name__)
@@ -21,40 +16,16 @@ class BlueSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     # def __init__(self):
-        # super(TemplateSkill, self).__init__(name="TemplateSkill")
+    #     super(TemplateSkill, self).__init__(name="TemplateSkill")
 
-        # Initialize working variables used within the skill.
-        # self.count = 0
-
-    # The "handle_xxxx_intent" function is triggered by Mycroft when the
-    # skill's intent is matched.  The intent is defined by the IntentBuilder()
-    # pieces, and is triggered when the user's utterance matches the pattern
-    # defined by the keywords.  In this case, the match occurs when one word
-    # is found from each of the files:
-    #    vocab/en-us/Hello.voc
-    #    vocab/en-us/World.voc
-    # In this example that means it would match on utterances like:
-    #   'Hello world'
-    #   'Howdy you great big world'
-    #   'Greetings planet earth'
     @intent_handler(IntentBuilder("BlueIntent").require("BlueKeyword"))
     def handle_blue_intent(self):
-        # In this case, respond by simply speaking a canned response.
-        # Mycroft will randomly speak one of the lines from the file
-        #    dialogs/en-us/hello.world.dialog
         self.enclosure.eyes_color(0, 0, 255)
-        self.settings["current_eye_color"] = [0, 0, 255]
+        # self.settings["current_eye_color"] = [0, 0, 255]
         self.speak_dialog("blue")
+        sleep(4)
+        self.enclosure.eyes_color(*self.settings["current_eye_color"])
 
-    def stop(self):
-        pass
-
-    # The "stop" method defines what Mycroft does when told to stop during
-    # the skill's execution. In this case, since the skill's functionality
-    # is extremely simple, there is no need to override it.  If you DO
-    # need to implement stop, you should return True to indicate you handled
-    # it.
-    #
     # def stop(self):
     #    return False
 
